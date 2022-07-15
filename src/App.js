7a2f38fbd4399f5f8e46b14d251cd237502acfc8
imenmarzchib <imenmarzouguichibani3@gmail.com>
2022-07-15 15:55:22 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { useEffect } from 'react';
+import UserList from './component/UserList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+ 
+  return (
+    <div className="App">
+    <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Cardd.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+
+function Cardd ({user}) {
+  return (
+    <div className='aa' >
+      
+    <Card  className='bb'>
+
+        <Card.Title >{user.name} </Card.Title>
+        <Card.Title>{user.username} </Card.Title>
+          <Card.Body>
+            <Card.Text> {user.email}</Card.Text>
+            <Card.Text>{user.address.street+" "+user.address.suite}</Card.Text>
+            <Card.Text>{user.address.city+" "+user.address.zipcode}</Card.Text>
+            <Card.Text>{user.address.geo.lat+" "+user.address.geo.lng}</Card.Text>
+          </Card.Body>
+        </Card>
+
+    </div>
+  )
+}
+
+export default  Cardd
